47f1e10e59f4c03ecf882977f63bfef873bb58be
smittytone <smittytone@icloud.com>
2020-05-01 13:31:18 +0100

    Update iss.py

--- ublox/iss.py
+++ ublox/iss.py
@@ -12,8 +12,6 @@ modem.set_debug(False)
 base_url = "http://api.open-notify.org"
 conn_open = False
 
-
-
 while True:
     try:
         # Open a data connection
@@ -24,32 +22,29 @@ while True:
         modem.send_command("AT+UHTTP=0")
 
         # Set the URL parameters: length and timeout
-        modem.send_command("AT+UHTTP=0,1\"" + base_url + "\"")
+        modem.send_command("AT+UHTTP=0,1,\"" + base_url + "\"")
 
         # Make the GET request
-        result = modem.send_command("AT+UHHTPC=0,1,\"/iss-now.json\",\"data.json\"", "+UUHTTPCR")
+        result = modem.send_command("AT+UHTTPC=0,1,\"/iss-now.json\",\"data.json\"", "+UUHTTPCR")
 
         if result[0] == "OK":
             # Check for HTTP error code
             result = result[1].split(": ")
             result = result[1].split(",")
-            if result[0] == "0":
-                response = modem.send_command("AT+QHTTPREAD", "OK")
+            if result[2] == "1":
+                response = modem.send_command("AT+URDFILE=\"data.json\"", "+URDFILE:")
+                response = response[1].split(": ")
+                response = response[1].split(",")
+                response = response[2]
+                data = json.loads(response)
 
-                # Parse the response
-                if response[0] == "OK":
-                    response = response[1].split("\r\n")[2]
-                    data = json.loads(response)
-
-                    if data["message"] == "success":
-                        print("ISS is at",data["iss_position"]["longitude"],",",data["iss_position"]["latitude"])
+                if data["message"] == "success":
+                    print("ISS is at",data["iss_position"]["longitude"],",",data["iss_position"]["latitude"])
                 else:
                     print("ISS location not retrieved")
-            else:
-                print("ISS location not retrieved")
 
         # Close the data connection
-        #modem.deactivate_context()
+        modem.deactivate_context()
         conn_open = False
 
         # Pause 1 minute
